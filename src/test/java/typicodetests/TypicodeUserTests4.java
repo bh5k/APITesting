486f8ad2063e214db4e0f29466ca996759ef9bb9
BackpackTrainer <bill@fairfieldco.com>
2023-11-06 11:24:38 -0500

    added directions for exercise 4

---
+++ src/test/java/typicodetests/TypicodeUserTests4.java
@@ -0,0 +1,20 @@
+package typicodetests;
+
+public class TypicodeUserTests4 {
+    /*
+    /posts/1/comments
+    /albums/1/photos
+    /users/1/albums
+    /users/1/todos
+    /users/1/posts
+
+    Above is a list of endpoints for jsonplaceholder.typicode.com
+    TODO create tests that validate these endpoints
+    TODO create classes for photos, albums, todos, comments and posts
+    TODO verify that the correct number of each are returned when you request all
+        HINT:  use the browser to send the request and look at the returned JSON to determine the number and content
+    TODO create tests that validate the correct content is returned when requesting a specific comment, photo, album, etc
+    TODO Play!  verify how many photos user 2 has, etc.  How many things can you think of to test?
+
+     */
+}
